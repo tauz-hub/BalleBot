602f4d222811b336d3ea96652bf5baac02d2390b
Tauã Santo <86664818+tauzxd@users.noreply.github.com>
2021-08-29 13:29:22 -0300

    Update Command.handler.js

--- src/commands/Command.handler.js
+++ src/commands/Command.handler.js
@@ -29,13 +29,11 @@ function genCommand(folder, returnCollection) {
             if (command.aliases) {
                 command.aliases.map((alias) => {
                     returnCollection.set(alias.toLowerCase(), command)
-                        //console.log(returnCollection)
                 })
             }
             command.name = command.name.toLowerCase()
             returnCollection.set(command.name, command);
             setCommandsInDatabase(command);
-            //  console.log(returnCollection)
 
         } catch (e) {
             console.log("error:", name, e)
